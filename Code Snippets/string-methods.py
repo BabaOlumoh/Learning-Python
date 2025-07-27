a6878f48366fd3e55ce8c5625598fcc45ef985cf
BabaOlumoh <olumohbabatunde@gmail.com>
2025-07-27 12:34:44 +0100

    string methods

---
+++ Code Snippets/string-methods.py
@@ -0,0 +1,10 @@
+#Strings Properties and Methods
+
+phrase = " Hello World "
+
+print(phrase.count("l")) #counts the instance of a character in a word, phrase
+print(phrase.strip()) #remove any whitespace in the beginning and end
+print("THIS IS IN LOWER".lower())
+print("this is in upper case".upper())
+print(phrase.replace("H","W"))
+print(phrase.split(","))
