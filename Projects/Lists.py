1c72cb6b9242372ec94eef8ef601df5ce37cc380
BabaOlumoh <olumohbabatunde@gmail.com>
2025-07-28 22:46:01 +0100

    List Menu v1

--- Projects/Lists.py
+++ Projects/Lists.py
@@ -1,4 +1,4 @@
-list_menu = []
+list_menu = ["pizza", "movies", "coding", "music", "football"]
 
 print("Welcome to your List \n")
 
@@ -13,15 +13,22 @@ elif stored_input == 2:
     print("Add a new item to the list")
     user_input_add = input()
     list_menu.append(user_input_add)
-    print("item added")
+    print("item added successfully")
 elif stored_input == 3:
     print("Remove an item from the list below")
     print(list_menu)
     user_input_remove = input()
-    list_menu.pop()
+    stored_user_input_remove = int(user_input_remove)
+    list_menu.pop(stored_user_input_remove)
     print("item removed successfully")
-
-
+elif stored_input == 4:
+    list_menu.sort()
+    print(f"List sorted alphabetically\n{list_menu}")
+elif stored_input == 5:
+    list_menu.reverse()
+    print(f"List shown in descending order\n{list_menu}")
+elif stored_input == 6:
+    print(list_menu[:3])
 else:
     print("Please enter a valid response")
 
