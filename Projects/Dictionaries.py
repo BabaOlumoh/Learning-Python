ba14f8537e3157b09ad2b51793e38dfd66d586c0
BabaOlumoh <olumohbabatunde@gmail.com>
2025-07-29 23:23:28 +0100

    Personal Info Organizer

---
+++ Projects/Dictionaries.py
@@ -0,0 +1,12 @@
+personal_info = {
+    "Name": "Mel",
+    "Age": "25",
+    "Hobby":"Reading"
+}
+
+print("\nWelcome to your Personal Info Organizer!\n")
+print("1. View Keys \n2. View Values \n3. View Items \n4. Add Info \n5. Remove Info")
+print()
+user_input = input("Choose an option (1-5): ")
+stored_user_input = int(user_input)
+
