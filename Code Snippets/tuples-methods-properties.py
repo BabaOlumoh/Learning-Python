8e365f9bf1c419dc543009822b11be43f7115c01
BabaOlumoh <olumohbabatunde@gmail.com>
2025-07-30 20:59:20 +0100

    tuples-methods-properties

---
+++ Code Snippets/tuples-methods-properties.py
@@ -0,0 +1,6 @@
+my_tuple = (1,"two", 3.5)
+print(my_tuple)
+my_tuple[0] #Tuples support indexing
+len(my_tuple) #You can check the length of the tuple
+my_tuple.count(3.5) #You can check how many instance of an object in tuples
+my_tuple.index(3.5) #You can check the index of an item 
