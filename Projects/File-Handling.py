eabc13a1657ad51920450766e5fb338790d86f64
BabaOlumoh <olumohbabatunde@gmail.com>
2025-08-03 17:33:18 +0100

    File-Handling.py

--- Projects/File-Handling.py
+++ Projects/File-Handling.py
@@ -1,7 +1,7 @@
 
-stored_entry = ("/Users/homebase/Desktop/Python/Udemy Course/Dependencies/stored_entry.txt")
+stored_entry = (("/Users/homebase/Desktop/Python/Udemy Course/Dependencies/diaryfile.txt"))
 myfavourite = ("/Users/homebase/Desktop/Python/Udemy Course/Dependencies/myfavourite.txt")
-diary_entry = stored_entry
+
 
 
 print("Welcome to your Daily Diary Logger")
@@ -10,18 +10,25 @@ print("1. Write Entry \n2. Read all Recorded Entry \n3. Add your Favourites \n4.
 menu_input = int(input("Choose between 1-5: ").strip())
 
 if menu_input == 1:
-    print("Record Activities")
-    user_diary_entry = input("Enter your activities for the day: ")
-    with open (diary_entry, 'a') as f:
-        f.write(f"{user_diary_entry}\n")
+    user_stored_entry = (input("Enter your activities for the day: "))
+    with open (stored_entry, 'a') as f:
+        f.write(f"{user_stored_entry}\n")
 elif menu_input == 2:
-    with open(diary_entry, 'r') as f:
+    with open(stored_entry, 'r') as f:
         print(f.read())
 elif menu_input == 3:
-    print
+    fav_input = tuple(input("Record your three favourites activities here: "))
+    with open(myfavourite, 'a') as f:
+        f.write(f"{fav_input}\n")
 elif menu_input == 4:
-    print
+    checker_input = input("Enter keywords: ")
+    with open(stored_entry, 'r') as f:
+        keyword = f.read()
+    if checker_input in keyword :
+        print(f"{checker_input} is present in the document provided")
+    else: print(f"{checker_input} is not present in the document")
 elif menu_input == 5:
-    print
-
+    with open(stored_entry, 'r') as f:
+        last_entry = list(f.read())
+        print(f"Your last entry is {last_entry[::-1]}")
 else: print("Invalid input")
