8690eb406b874242c6e16bcdb8e41e32b93bac88
BabaOlumoh <olumohbabatunde@gmail.com>
2025-08-03 16:37:15 +0100

    Write and Read functions added

---
+++ Projects/File-Handling.py
@@ -0,0 +1,27 @@
+
+stored_entry = ("/Users/homebase/Desktop/Python/Udemy Course/Dependencies/stored_entry.txt")
+myfavourite = ("/Users/homebase/Desktop/Python/Udemy Course/Dependencies/myfavourite.txt")
+diary_entry = stored_entry
+
+
+print("Welcome to your Daily Diary Logger")
+print("1. Write Entry \n2. Read all Recorded Entry \n3. Add your Favourites \n4. Search for Keyword \n5. Show the Last Recorded Entry \n")
+
+menu_input = int(input("Choose between 1-5: ").strip())
+
+if menu_input == 1:
+    print("Record Activities")
+    user_diary_entry = input("Enter your activities for the day: ")
+    with open (diary_entry, 'a') as f:
+        f.write(f"{user_diary_entry}\n")
+elif menu_input == 2:
+    with open(diary_entry, 'r') as f:
+        print(f.read(diary_entry))
+elif menu_input == 3:
+    print
+elif menu_input == 4:
+    print
+elif menu_input == 5:
+    print
+
+else: print("Invalid input")
