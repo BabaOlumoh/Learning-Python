b38174fdedc26abddc524f3fa7ff89d12c078fda
BabaOlumoh <olumohbabatunde@gmail.com>
2025-08-12 23:12:23 +0100

    Update function added

--- Projects/Functions.py
+++ Projects/Functions.py
@@ -2,7 +2,7 @@
 file = {'Tunde':90, 'Tade':70}
 
 def menu_choice():
-    user_menu = int(input("Choose between 1-5: "))
+    user_menu = int(input("Choose between 1-6: "))
     return user_menu
 
 def user_input():
@@ -13,7 +13,7 @@ def user_input():
 
 while True:
     print("======== Student Grade Manager ========\n")
-    print("1. Add Student \n2. View Students \n3. Search for Student \n4. Remove Student \n5. Exit")
+    print("1. Add Student \n2. View Students \n3. Search for Student \n4. Remove Student \n5. Update Grade \n6. Exit")
     choice = menu_choice()
 
     if choice == 1:
@@ -44,8 +44,19 @@ while True:
             file.pop(get_remove_name)
             print(f"{get_remove_name} has been removed")
             print(f"The updated database \n{file}")
+        else: print(f"{get_remove_name} not in database")
     elif choice == 5:
+        print("Enter student name below: ")
         get_update_name = user_input()
+        
+        if get_update_name in file:
+            print("Enter new grade below: ")
+            get_update_grade = int(user_input())
+            if get_update_grade >= 0 and get_update_grade  <= 100:
+                file[get_update_name] = get_update_grade
+            else: print("The allowed grade input is 0-100")
+            print(f"The updated database \n{file}")
+        else: print(f"{get_update_name} not found in datebase")
     elif choice == 6:
         print("Program Closed! \n")
         break
