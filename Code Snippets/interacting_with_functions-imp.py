2280c8fe00a27e2f99dbbcf89e159d802ea32481
BabaOlumoh <olumohbabatunde@gmail.com>
2025-08-13 22:01:02 +0100

    Interacting with Function - adding functions

--- Code Snippets/interacting_with_functions-imp.py
+++ Code Snippets/interacting_with_functions-imp.py
@@ -9,7 +9,20 @@ def shuffle_list(mylist):
 def user_guess():
     guess = ""
     while guess not in ['0', '1', '2']:
-        guess = input("Enter 0,1 or 2")
+        guess = input("Enter 0, 1 or 2: ")
     
     return int(guess)
 
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+        print("Correct, you've won this round!")
+        print(mylist)
+    else:
+        print("Wrong guess!")
+        print(mylist)
+
+mixedup_list = shuffle_list(mylist)
+
+guess = user_guess()
+
+check_guess(mixedup_list, guess)
