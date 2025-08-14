2589e8acb3ff317630ce39cbfd752459e5f8b3b1
BabaOlumoh <olumohbabatunde@gmail.com>
2025-08-14 19:38:13 +0100

    Three Cup Monte game

--- Projects/Three-Cup-Monte.py
+++ Projects/Three-Cup-Monte.py
@@ -1,11 +1,31 @@
 from random import shuffle
 
-mylist = [' ', '0', ' ']
+mylist = [' ', 'O', ' ']
+
 
 def shuffle_list(mylist):
     shuffle(mylist)
     return(mylist)
 
-print(shuffle_list(mylist))
+def player_guess():
+    guess = ''
+    while guess not in ['0','1','2']:
+        guess = input("Enter 0, 1 or 2: ")
+    return int(guess)
 
-def 
+def check_guess(mylist, guess):
+    if mylist[guess] == 'O':
+        print("Correct! You've won this round")
+        print(mylist)
+        
+    else:
+        print("Wrong Guess!")
+        print(mylist)
+    return (mylist, guess)
+
+while True:
+        mylist = shuffle_list(mylist)
+
+        guess = player_guess()
+
+        mixed = check_guess(mylist, guess)
