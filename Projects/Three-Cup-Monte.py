bf56fbf0087d0d6093c3d8f17bbd12149219bdf9
BabaOlumoh <olumohbabatunde@gmail.com>
2025-08-17 13:07:28 +0100

    Three Cup Monte game with loop and score counter

--- Projects/Three-Cup-Monte.py
+++ Projects/Three-Cup-Monte.py
@@ -1,31 +1,44 @@
 from random import shuffle
 
-mylist = [' ', 'O', ' ']
-
-
+score = 0 
 def shuffle_list(mylist):
     shuffle(mylist)
-    return(mylist)
+    return (mylist)
+
 
 def player_guess():
     guess = ''
-    while guess not in ['0','1','2']:
-        guess = input("Enter 0, 1 or 2: ")
+    while guess not in ['0', '1', '2', '6']:
+        guess = input("Enter 0, 1, 2 or 6 to quit: ")
+    if guess == '6':
+        print("Game closed")
+        return None
     return int(guess)
 
+
 def check_guess(mylist, guess):
+    
     if mylist[guess] == 'O':
         print("Correct! You've won this round")
+        global score
+        score +=1
         print(mylist)
-        
+
     else:
         print("Wrong Guess!")
         print(mylist)
     return (mylist, guess)
 
+
 while True:
-        mylist = shuffle_list(mylist)
+    mylist = [' ', 'O', ' ']
+    mylist = shuffle_list(mylist)
+    guess = player_guess()
+    counter =+ 1
 
-        guess = player_guess()
-
-        mixed = check_guess(mylist, guess)
+    if guess is None:
+        print(f"Player won {score} rounds")
+        break
+    mixed = check_guess(mylist, guess)
+    print(f"Player has won {score} rounds")
+    
