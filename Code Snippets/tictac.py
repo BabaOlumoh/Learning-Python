224f77b5ff3608d773e0810e5607596befae74a9
BabaOlumoh <olumohbabatunde@gmail.com>
2025-09-07 19:44:05 +0100

    Milestone Project 1 warmup

--- Code Snippets/tictac.py
+++ Code Snippets/tictac.py
@@ -15,3 +15,29 @@ def user_choice():
 
 
 #Tutorial version
+
+def user_choice():
+    #VARIBLES
+
+    #initial
+    choice = 'WRONG'
+    acceptance_range = range(0,11)
+    within_range = False
+
+    #TWO CONDITIONS TO CHECK
+    #DIGIT OR WITHIN_RANGE == FALSE
+    
+    while choice.isdigit() == False or within_range == False:
+        choice = (input("Enter a number between 0-10: "))
+
+        #DIGIT CHECK
+        if choice.isdigit() == False:
+            print("Sorry that's not a digit!")
+
+        #RANGE CHECK
+        if choice.isdigit() == True:
+            if int(choice) in acceptance_range:
+                within_range = True
+            else:
+                print("You're out of acceptable range")
+                within_range = False
