05ccec9f0f5ad4e3b0ff7408d7f5736d162402d5
BabaOlumoh <olumohbabatunde@gmail.com>
2025-09-16 09:44:08 +0100

    error handling and exceptions homework problems

--- Code Snippets/error-handling-and-exceptions.py
+++ Code Snippets/error-handling-and-exceptions.py
@@ -12,3 +12,36 @@ def ask_for_it():
             print("This block will always run")
 
 ask_for_it()
+
+for i in ['a','b',3]:
+    try:
+        print(i**2)
+    except: 
+        print("This is invalid \n Try again!")
+        continue
+    else: 
+        print("Correct")
+        break
+
+x = 5
+y = 0
+try: 
+    z = x/y
+except ZeroDivisionError: 
+    print("Can't be divided by zero")
+finally: 
+    print("All Done")
+
+def ask():
+    while True:
+        try:
+            sq = int(input("Input an integer: "))
+            sq*=sq
+        except: 
+            print("An error occurred! Please try again!")
+            continue
+        else:
+            print(f"Thank you, your number squared is:  {sq}")
+            break
+
+ask()
