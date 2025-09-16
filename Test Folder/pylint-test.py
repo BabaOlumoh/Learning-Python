d1503a75f5ffb3eaab3ac3aa661acb8fd8294448
BabaOlumoh <olumohbabatunde@gmail.com>
2025-09-16 10:37:37 +0100

    Unit Testing

---
+++ Test Folder/pylint-test.py
@@ -0,0 +1,15 @@
+'''
+A very simple scripy
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+#pylint simple1.py -r y
--- Test Folder/simple1.py
+++
@@ -1,7 +0,0 @@
-a = 1
-b = 2
-print(a)
-print(B)
-
-#pylint simple1.py -r y
-
