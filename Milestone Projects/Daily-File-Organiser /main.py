3e7eae74e584e2a4e727c21da65b404892d74702
BabaOlumoh <olumohbabatunde@gmail.com>
2025-10-03 09:04:47 +0100

    Daily File Organiser(log report implemented)

--- Milestone Projects/Daily-File-Organiser /main.py
+++ Milestone Projects/Daily-File-Organiser /main.py
@@ -3,91 +3,105 @@ import os
 import shutil
 
 #Getting time for file name
-getdate = datetime.datetime.now()
-file_name = getdate.strftime("%Y%m%d__%H%M")
+date_init = datetime.datetime.now()
+get_date = date_init.strftime("%Y%m%d_%H%M")
 
 #File creation and error handling for folders
-#PDF
+#PDF Folder
 pdf = os.path.join(os.getcwd(), "Downloads/PDFs")
 try:
     os.makedirs(pdf, exist_ok = False)
-    print("File created successfully")
+    print(f"{os.path.basename(pdf)} folder created successfully")
 except OSError as error:
-    print("File already exist!")
+    print(f"{os.path.basename(pdf)} folder already exist!")
 
-#Images
+#Images Folder
 images = os.path.join(os.getcwd(), "Downloads/Images")
 try:
     os.makedirs(images, exist_ok = False)
-    print("File created successfully")
+    print(f"{os.path.basename(images)} folder created successfully")
 except OSError as error:
-    print("File already exist!")
+    print(f"{os.path.basename(images)} folder already exist!")
     
-#Videos
+#Videos Folder
 videos = os.path.join(os.getcwd(), "Downloads/Videos")
 try:
     os.makedirs(videos, exist_ok = False)
-    print("File created successfully")
+    print(f"{os.path.basename(videos)} folder created successfully")
 except OSError as error:
-    print("File already exist!")
+    print(f"{os.path.basename(videos)} folder already exist!")
 
-#Documents
+#Documents Folder
 documents = os.path.join(os.getcwd(), "Downloads/Documents")
 try:
     os.makedirs(documents, exist_ok = False)
-    print("File created successfully")
+    print(f"{os.path.basename(documents)} folder created successfully")
 except OSError as error:
-    print("File already exist!")
+    print(f"{os.path.basename(documents)} folder already exist!")
 
-#Zips
+#Zip Folder
 zips = os.path.join(os.getcwd(), "Downloads/Zips")
 try:
     os.makedirs(zips, exist_ok = False)
-    print("File created successfully")
+    print(f"{os.path.basename(zips)} folder created successfully")
 except OSError as error:
-    print("File already exist!")
-    
-#Others
+    print(f"{os.path.basename(zips)} folder already exist!")
+
+#Others Folder
 others = os.path.join(os.getcwd(), "Downloads/Others")
 try:
     os.makedirs(others, exist_ok = False)
-    print("File created successfully")
+    print(f"{os.path.basename(others)} folder created successfully")
 except OSError as error:
-    print("File already exist!")
+    print(f"{os.path.basename(others)} folder already exist!")
+
+#Log Folder
+log = os.path.join(os.getcwd(), "Log")
+try:
+    os.makedirs(log, exist_ok = False)
+    print(f"{os.path.basename(log)} folder created successfully")
+except OSError as error:
+    print(f"{os.path.basename(log)} folder already exist!")
+
+#Backup Folder
+backup = os.path.join(os.getcwd(), "Backup")
+try:
+    os.makedirs(backup, exist_ok = False)
+    print(f"{os.path.basename(backup)} folder created successfully")
+except OSError as error:
+    print(f"{os.path.basename(backup)} folder already exist!")
 
 #Moving files
 downloads = os.path.join(os.getcwd(), "Downloads")
+with open(f"Log/{get_date}logfile", "a") as f:
+    for file in os.listdir(downloads):
+        source = os.path.join(downloads, file)
 
-for file in os.listdir(downloads):
-    source = os.path.join(downloads, file)
-    if file.endswith((".mp4", ".mkv", ".mov")):
-        source = os.path.join(downloads, file)
-        destination = os.path.join(videos, file)
-        shutil.move(source, destination)
-        print(f"{file} moved to {os.path.basename(videos)}")
-    elif file.endswith((".jpg", ".jpeg", ".png", ".gif")):
-        source = os.path.join(downloads, file)
-        destination = os.path.join(images, file)
-        shutil.move(source, destination)
-        print(f"{file} moved to {os.path.basename(images)}")
-    elif file.endswith((".pdf")):
-        source = os.path.join(downloads, file)
-        destination = os.path.join(pdf, file)
-        shutil.move(source, destination)
-        print(f"{file} moved to {os.path.basename(pdf)}")
-    elif file.endswith((".docx", ".txt", ".csv")):
-        source = os.path.join(downloads, file)
-        destination = os.path.join(documents, file)
-        shutil.move(source, destination)
-        print(f"{file} moved to {os.path.basename(documents)}")
-    elif file.endswith((".zip", ".rar", ".7z")):
-        source = os.path.join(downloads, file)
-        destination = os.path.join(zips, file)
-        shutil.move(source, destination)
-        print(f"{file} moved to {os.path.basename(zips)}")
-    elif os.path.isfile(source): #To ensure only a file is movedA
-        destination = os.path.join(others, file)
-        shutil.move(source, destination)
-        print(f"{file} moved to {os.path.basename(others)}")
-    else:
-        print("No found moved!")
+        #To ensure only a file is moved
+        if not os.path.isfile(source):
+            continue
+            
+        if file.endswith((".mp4", ".mkv", ".mov")):
+            destination = os.path.join(videos, file)
+            shutil.move(source, destination)
+            f.write(f"{file} moved to {os.path.basename(videos)} on {get_date}\n")
+        elif file.endswith((".jpg", ".jpeg", ".png", ".gif")):
+            destination = os.path.join(images, file)
+            shutil.move(source, destination)
+            f.write(f"{file} moved to {os.path.basename(images)} on {get_date}\n")
+        elif file.endswith((".pdf")):
+            destination = os.path.join(pdf, file)
+            shutil.move(source, destination)
+            f.write(f"{file} moved to {os.path.basename(pdf)} on {get_date}\n")
+        elif file.endswith((".docx", ".txt", ".csv")):      
+            destination = os.path.join(documents, file)
+            shutil.move(source, destination)
+            f.write(f"{file} moved to {os.path.basename(documents)} on {get_date}\n")
+        elif file.endswith((".zip", ".rar", ".7z")):    
+            destination = os.path.join(zips, file)
+            shutil.move(source, destination)
+            f.write(f"{file} moved to {os.path.basename(zips)}  on {get_date}\n")
+        else: 
+            destination = os.path.join(others, file)
+            shutil.move(source, destination)
+            f.write(f"{file} moved to {os.path.basename(others)}  on {get_date}\n")
